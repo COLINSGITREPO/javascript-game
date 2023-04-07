18766874b74afaaf3fd9997e565736bf18ebfc45
Colin Shaw <colin.shaw400@outlook.com>
2023-04-07 14:43:40 +0100

    add image and node_app container to deploy file

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Replace with your actual application name
-APP_NAME="SimpleApplication"
+APP_NAME="javascript-game"
 
 # Stop the application if it's already running
 #!/bin/bash
 
 # Replace with your actual application name
-APP_NAME="SimpleApplication"
+APP_NAME="javascript-game"
 
 # Stop the application if it's already running
 if [ -f /var/run/${APP_NAME}.pid ]; then
@@ -23,10 +23,38 @@ git clone https://github.com/COLINSGITREPO/javascript-game.git /var/www/${APP_NA
 # Change to the repository directory
 cd /var/www/${APP_NAME}
 
+#check if there is instance running with the image name that is been deployed
+CURRENT_INSTANCE=$(docker ps -a-q --filter ancestor="$IMAGE_NAME" --format="{{.ID}}")
+
+# if instance existce stop the instance
+if [ "$CURRENT_INSTANCE" ]
+then
+  docker rm $(docker stop $CURRENT_INSTANCE)
+fi
+
+# pull down the instance from docker hub
+docker pull $IMAGE_NAME
+
+# check id docker container exists with the name of the node_app if not remove the container
+CONTAINER_EXISTS=$(docker ps -a | grep node_app)
+if [ "$CONTAINER_EXISTS" ]
+then 
+  docker rm node_app
+fi
+
+# create a container called node_app that is avilable on port 8443 from docker image
+docker create -p 8443:8443 --name node_app $IMAGE_NAME
+
 # Install any required dependencies
 sudo npm install
 echo $PRIVATE_KEY > CA1.pem
 echo $SERVER > server.crt
+# add private key to node_app
+docker cp ./privatekey.pem node_app:/privatekey.pem
+# add the server key to node_app
+docker cp ./server.crt node_app:/server.crt
+# start the node_app container
+docker start node_app
 # Build the application
 sudo npm run build
 
