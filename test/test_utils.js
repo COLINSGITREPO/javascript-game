6cac6c25652cc9a25960e835d2353343e9e8c9bc
Tamara DBre <breversemail@gmail.com>
2023-04-07 17:32:48 +0100

    test fix

--- test/test_utils.js
+++ test/test_utils.js
@@ -1,154 +1,89 @@
 // Import JSDOM from the 'jsdom' package
 
 const jsdom = require('jsdom');
-
 const { JSDOM } = jsdom;
-
-
-
 
 // Set up a fake DOM environment
 
 const fakeDom = () => {
+const dom = new JSDOM(
 
-  const dom = new JSDOM(
+ `<!doctype html>
+ <html>
+    <body>
+        <div>
+            <p id="player-choice"></p>
+            <p id="computer-choice"></p>
+            <p id="winner"></p>
+            <div id="results-choice"></div>
+        </div>
+    </body>
+ </html>`
+ );
 
-    `<!doctype html>
+ global.window = dom.window;
 
-      <html>
-
-        <body>
-
-          <div>
-
-            <p id="player-choice"></p>
-
-            <p id="computer-choice"></p>
-
-            <p id="winner"></p>
-
-            <div id="results-choice"></div>
-
-          </div>
-
-        </body>
-
-      </html>`
-
-    );
-
-
-
-
-  global.window = dom.window;
-
-  global.document = dom.window.document;
+ global.document = dom.window.document;
 
 };
 
-
-
-
 fakeDom();
-
-
-
-
 // Import the functions to test
 
 const assert = require('assert');
 
 const { onButtonClick, getResults } = require('../assets/js/app.js');
 
+ describe('onButtonClick', () => {
 
+ beforeEach(() => {
 
+ fakeDom();
 
-describe('onButtonClick', () => {
+ });
 
-  beforeEach(() => {
+ it('should return "Player wins" when player chooses "rock" and computer chooses "scissors"', () => {
 
-    fakeDom();
+ const playerChoice = 'rock';
 
-  });
+ const computerChoice = 'scissors';
 
+ const expectedResult = 'Rock crushes Scissors!! Player Wins';
+ const result = getResults(playerChoice, computerChoice);
+ assert.strictEqual(result, expectedResult);
 
+ assert.strictEqual(result, document.getElementById("results-choice").innerHTML);
 
+ });
 
-  it('should return "Player wins" when player chooses "rock" and computer chooses "scissors"', () => {
+ it('should return "Computer wins" when player chooses "rock" and computer chooses "paper"', () => {
+ const playerChoice = 'rock';
+ const computerChoice = 'paper';
 
-    const playerChoice = 'rock';
+ const expectedResult = 'Paper covers Rock! Computer Wins';
+ const result = getResults(playerChoice, computerChoice);
 
-    const computerChoice = 'scissors';
-
-    const expectedResult = 'Rock crushes Scissors!! Player Wins';
-
-
-
-
-    const result = getResults(playerChoice, computerChoice);
-
-
-
-
-    assert.strictEqual(result, expectedResult);
-
-    assert.strictEqual(result, document.getElementById("results-choice").innerHTML);
-
-  });
-
-
-
-
-  it('should return "Computer wins" when player chooses "rock" and computer chooses "paper"', () => {
-
-    const playerChoice = 'rock';
-
-    const computerChoice = 'paper';
-
-    const expectedResult = 'Paper covers Rock! Computer Wins';
+    assert.strictEqual(result, expectedResult);
+    assert.strictEqual(result, document.getElementById("results-choice").innerHTML);
+ });
 
 
 
 
-    const result = getResults(playerChoice, computerChoice);
+ it('should return "Tie" when player chooses "rock" and computer also chooses "rock"', () => {
 
+    const playerChoice = 'rock';
 
+    const computerChoice = 'rock';
+    const expectedResult = 'This round is a tie!';
 
+ const result = getResults(playerChoice, computerChoice);
+ assert.strictEqual(result, expectedResult);
 
-    assert.strictEqual(result, expectedResult);
+ assert.strictEqual(result, document.getElementById("results-choice").innerHTML);
 
-    assert.strictEqual(result, document.getElementById("results-choice").innerHTML);
+ });
 
-  });
-
-
-
-
-  it('should return "Tie" when player chooses "rock" and computer also chooses "rock"', () => {
-
-    const playerChoice = 'rock';
-
-    const computerChoice = 'rock';
-
-    const expectedResult = 'This round is a tie!';
-
-
-
-
-    const result = getResults(playerChoice, computerChoice);
-
-
-
-
-    assert.strictEqual(result, expectedResult);
-
-    assert.strictEqual(result, document.getElementById("results-choice").innerHTML);
-
-  });
-
-
-
-
-  // Add more tests here for the remaining choices: paper, scissors, lizard, and spock
+ // Add more tests here for the remaining choices: paper, scissors, lizard, and spock
 
 });
